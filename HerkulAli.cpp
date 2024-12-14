49bcddfb9ebb85d02199e6397a3625a05961cd31
SMMousavi <sarbaz14zaman@gmail.com>
2024-12-14 09:30:28 +0330

    create HashTable

--- HerkulAli.cpp
+++ HerkulAli.cpp
@@ -8,6 +8,16 @@ struct Node {
     int index;
 };
 
+class HashTable{
+private:
+    int size;
+    Node* Arr;
+
+public:
+    HashTable(int s): size(s), Arr(new Node[s]) {}
+
+};
+
 int main(){
     string text;
     getline(cin, text);
