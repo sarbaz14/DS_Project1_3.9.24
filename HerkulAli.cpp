ca8028a6e5efbca333df920462c0d8f4cfeef352
SMMousavi <sarbaz14zaman@gmail.com>
2024-12-14 11:19:54 +0330

    afjodan search

--- HerkulAli.cpp
+++ HerkulAli.cpp
@@ -34,6 +34,14 @@ private:
         }
     }
 
+    // int searchIndex(vector<int>* indexsInput, int size){
+    //     for (int i = 0; i < size; i++){
+    //         for (int j = 0; j < indexsInput[i].size(); j++){
+    //             int index = indexsInput[i][j].index;
+    //         }
+    //     }
+    // }
+
 public:
     void Input(string text){
         for(int i = 0; i < text.length(); i++){
@@ -41,11 +49,42 @@ public:
         }
     }
 
+    void Search(string* inputs, int size){
+        for (int i = 0; i < size; i++){
+            string input = inputs[i];
+
+            vector<int> indexsInput[input.length()];
+            for (int j = 0; j < input.length(); j++){
+                int index = hashFunction(input[j]);
+
+                Node* current = Arr[index];
+                while(current->next != nullptr){
+                    indexsInput[j].push_back(current->index);
+                    current =current->next;
+                }
+            }
+
+
+
+        }
+
+    }
 };
 
 int main(){
+    HashTable hashTable;
     string text;
     getline(cin, text);
+
+    hashTable.Input(text);
     
+    int num;
+    cin >> num;
+    string inputs[num];
+    for (int i = 0; i < num; i++){
+        getline(cin, inputs[i]);
+    }
+
+
 
 }
